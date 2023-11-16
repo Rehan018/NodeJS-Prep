72bc9306fd19026e53e8034ff53ef80f74ded416
Md Rehan Alam <76740248+Rehan018@users.noreply.github.com>
2023-11-16 14:51:59 +0530

    Add files via upload

--- youtubereturn.js
+++ youtubereturn.js
@@ -1,23 +1,60 @@
-const express = require('express');
-const app = express();
-const port = 3000;
+const http = require('http');
+const fs = require('fs');
 
-app.get('/', (req, res) => {
-  res.send('Welcome to my Node Js project');
+const server = http.createServer((req, res) => {
+    const url = req.url;
+    const method = req.method;
+
+    if (url === '/') {
+        res.write('<html>');
+        res.write('<head><title>Enter Message</title></head>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+        res.write('</html>');
+        return res.end();
+    }
+
+    if (url === '/message' && method === 'POST') {
+        fs.writeFileSync('message.txt', 'DUMMY');
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        return res.end();
+    }
+
+    // Handle custom responses based on the URL
+    if (url === '/home') {
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html>');
+        res.write('<head><title>Welcome home</title></head>');
+        res.write('<body><h1>Welcome home</h1></body>');
+        res.write('</html>');
+        return res.end();
+    }
+
+    if (url === '/about') {
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html>');
+        res.write('<head><title>About Us</title></head>');
+        res.write('<body><h1>Welcome to About Us page</h1></body>');
+        res.write('</html>');
+        return res.end();
+    }
+
+    if (url === '/node') {
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html>');
+        res.write('<head><title>Node.js Project</title></head>');
+        res.write('<body><h1>Welcome to my Node.js project</h1></body>');
+        res.write('</html>');
+        return res.end();
+    }
+
+    // Default response for other URLs
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>My first page</title></head>');
+    res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
+    res.write('</html>');
+    res.end();
 });
 
-app.get('/home', (req, res) => {
-  res.send('Welcome home');
-});
-
-app.get('/about', (req, res) => {
-  res.send('Welcome to About Us page');
-});
-
-app.get('/node', (req, res) => {
-  res.send('Welcome to my Node Js project');
-});
-
-app.listen(port, () => {
-  console.log(`Server is listening on port ${port}`);
-});
+server.listen(3000);
