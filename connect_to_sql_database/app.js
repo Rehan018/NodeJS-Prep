37a373bcdc9e808be286a0fda6b618c17bf8083b
Md Rehan Alam <76740248+Rehan018@users.noreply.github.com>
2023-11-21 15:32:01 +0530

    Add files via upload

--- connect_to_sql_database/app.js
+++ connect_to_sql_database/app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
-const db=require('./util/database');
+const sequelize=require('./util/database');
 
 const app = express();
 
@@ -14,7 +14,7 @@ app.set('views', 'views');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-db.execute('SELECT * FROM products')
+sequelize.query('SELECT * FROM products',{type:sequelize.QueryTypes.SELECT })
 .then(result =>{
     console.log(result[0],result[1]);
 }).catch(err =>{
@@ -28,7 +28,12 @@ app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 
-
 app.use(errorController.get404);
 
-app.listen(3000);
+sequelize.sync().then(result =>{
+    console.log(result);
+    app.listen(3000);
+}).catch(err =>{
+    console.log(err);
+});
+
--- connect_to_sql_database/util/database.js
+++ connect_to_sql_database/util/database.js
@@ -1,9 +1,19 @@
-const mysql=require('mysql2');
+// const mysql=require('mysql2');
 
-const pool=mysql.createPool({
-    host:'localhost',
-    user:'root',
-    database:'node_complete'
+// const pool=mysql.createPool({
+//     host:'localhost',
+//     user:'root',
+//     database:'node_complete'
+// });
+
+// module.exports=pool.promise();
+
+
+const Sequelize=require('sequelize');
+
+const sequelize=new Sequelize('node_complete','root','',{
+dialect:'mysql',
+host:'localhost'
 });
 
-module.exports=pool.promise();
+module.exports=sequelize;
